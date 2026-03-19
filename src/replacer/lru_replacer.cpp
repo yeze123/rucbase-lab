c69eafdd963d7d9a8db6a36647aca3ab8ae789d9
yeze123 <2428521318@qq.com>
2026-03-19 17:45:37 +0800

    Update lru_replacer.cpp

--- src/replacer/lru_replacer.cpp
+++ src/replacer/lru_replacer.cpp
@@ -12,7 +12,7 @@ See the Mulan PSL v2 for more details. */
 
 LRUReplacer::LRUReplacer(size_t num_pages) { max_size_ = num_pages; }
 
-LRUReplacer::~LRUReplacer() = default;  
+LRUReplacer::~LRUReplacer() = default;
 
 /**
  * @description: 使用LRU策略删除一个victim frame，并返回该frame的id
@@ -20,14 +20,19 @@ LRUReplacer::~LRUReplacer() = default;
  * @return {bool} 如果成功淘汰了一个页面则返回true，否则返回false
  */
 bool LRUReplacer::victim(frame_id_t* frame_id) {
-    // C++17 std::scoped_lock
-    // 它能够避免死锁发生，其构造函数能够自动进行上锁操作，析构函数会对互斥量进行解锁操作，保证线程安全。
-    std::scoped_lock lock{latch_};  //  如果编译报错可以替换成其他lock
+    std::scoped_lock lock{latch_};
 
-    // Todo:
-    //  利用lru_replacer中的LRUlist_,LRUHash_实现LRU策略
-    //  选择合适的frame指定为淘汰页面,赋值给*frame_id
+    // LRU淘汰：链表最后一个就是最久未使用
+    if (LRUlist_.empty()) {
+        return false;
+    }
 
+    // 获取最后一个frame
+    frame_id_t victim_frame = LRUlist_.back();
+    LRUlist_.pop_back();
+    LRUhash_.erase(victim_frame);
+
+    *frame_id = victim_frame;
     return true;
 }
 
@@ -37,9 +42,13 @@ bool LRUReplacer::victim(frame_id_t* frame_id) {
  */
 void LRUReplacer::pin(frame_id_t frame_id) {
     std::scoped_lock lock{latch_};
-    // Todo:
-    // 固定指定id的frame
-    // 在数据结构中移除该frame
+
+    // 从LRU列表中移除，不能被淘汰
+    auto it = LRUhash_.find(frame_id);
+    if (it != LRUhash_.end()) {
+        LRUlist_.erase(it->second);
+        LRUhash_.erase(it);
+    }
 }
 
 /**
@@ -47,9 +56,17 @@ void LRUReplacer::pin(frame_id_t frame_id) {
  * @param {frame_id_t} frame_id 取消固定的frame的id
  */
 void LRUReplacer::unpin(frame_id_t frame_id) {
-    // Todo:
-    //  支持并发锁
-    //  选择一个frame取消固定
+    std::scoped_lock lock{latch_};
+
+    // 如果已经存在，先删除旧位置
+    auto it = LRUhash_.find(frame_id);
+    if (it != LRUhash_.end()) {
+        LRUlist_.erase(it->second);
+    }
+
+    // 插入到链表头部（最近使用）
+    LRUlist_.push_front(frame_id);
+    LRUhash_[frame_id] = LRUlist_.begin();
 }
 
 /**
